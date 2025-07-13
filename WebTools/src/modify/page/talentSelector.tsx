16b9a72d5f83465828a9cfe4925ea73e4accdb9d
BC Holmes <github@bcholmes.org>
2025-07-13 18:58:22 -0400

    Ooops. Forgot to add this file

---
+++ WebTools/src/modify/page/talentSelector.tsx
@@ -0,0 +1,27 @@
+import { SelectedTalent } from "../../common/selectedTalent";
+import { CheckBox } from "../../components/checkBox";
+
+interface ITalentSelectorProperties {
+
+    values: SelectedTalent[];
+    isChecked: (t: SelectedTalent, i: number) => boolean;
+    onSelect: (t: SelectedTalent, i: number) => void;
+}
+
+export const TalentSelector: React.FC<ITalentSelectorProperties> =
+    ({onSelect, values, isChecked}) => {
+
+    return (<table className="selection-list">
+        <tbody>
+            {values.map((s, i) => {
+                return (<tr key={i}>
+                    <td className="selection-header-small">{s.displayName}</td>
+                    <td className="text-end">
+                        <CheckBox text="" value={s} isChecked={isChecked(s, i)}
+                            onChanged={(val) => onSelect(s, i)} />
+                    </td>
+                </tr>);
+            })}
+        </tbody>
+    </table>);
+}
