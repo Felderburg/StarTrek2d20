e50c4d376b33260f54560810f8af8fc09e47a60a
BC Holmes <github@bcholmes.org>
2026-03-20 16:53:23 -0400

    New release

--- WebTools/src/components/news.tsx
+++ WebTools/src/components/news.tsx
@@ -14,6 +14,12 @@ const News: React.FC<INewsProperties> = ({showModal, onClose}) => {
 
     return (
         <Modal size="lg" show={showModal} onClose={() => onClose()} header="What's New?">
+            <b>v1.260320</b>
+            <ul>
+                <li>
+                    Additional bug fixes.
+                </li>
+            </ul>
             <b>v1.260315</b>
             <ul>
                 <li>
@@ -114,12 +120,6 @@ const News: React.FC<INewsProperties> = ({showModal, onClose}) => {
             <ul>
                 <li>
                     Minor content revisions.
-                </li>
-            </ul>
-            <b>v1.250917</b>
-            <ul>
-                <li>
-                    Some quick fixes.
                 </li>
             </ul>
 
