2a02e219b449d4f1f4b86f2543109f2cdbd0d1f4
Kevin Baird <kevincbaird@gmail.com>
2026-08-03 10:09:48 -0400

    Speed up test suite via ts-jest isolatedModules

--- WebTools/jest.config.js
+++ WebTools/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    transform: {'^.+\\.tsx?$': 'ts-jest'},
+    transform: {'^.+\\.tsx?$': ['ts-jest', {isolatedModules: true}]},
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
